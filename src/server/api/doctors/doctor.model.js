9019392bca1f1aa3da6ed8dcb1f939347b62890a
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-04-27 21:40:30 -0400

    Add helpers to model

--- src/server/api/doctors/doctor.model.js
+++ src/server/api/doctors/doctor.model.js
@@ -1,4 +1,29 @@
+/* eslint object-shorthand: 0 func-names: 0 */
+
 import Sequelize from 'sequelize';
+
+function arrayToJSON(array) {
+  return JSON.stringify(array);
+}
+
+function emailListToJSON(model) {
+  model.emailList = arrayToJSON(model.emailList);
+}
+
+function ratingBreakdownToJSON(model) {
+  model.ratingBreakdown = arrayToJSON(model.ratingBreakdown);
+}
+
+function safelyParseJSON(json) {
+  if (!json) {
+    return '';
+  }
+  try {
+    return JSON.parse(json);
+  } catch (err) {
+    console.log('Could not parse JSON', json, err);
+  }
+}
 
 export const Doctor = {
   name: 'doctors',
@@ -9,8 +34,19 @@ export const Doctor = {
     rating: Sequelize.DECIMAL,
     bestRating: Sequelize.DECIMAL,
     worstRating: Sequelize.DECIMAL,
-    url: Sequelize.STRING,
-    emailList: Sequelize.TEXT
+    url: { type: Sequelize.STRING, unique: true },
+    emailList: {
+      type: Sequelize.TEXT,
+      get: function () {
+        safelyParseJSON(this.getDataValue('emailList'));
+      }
+    }
+  },
+  methods: {
+    hooks: {
+      beforeCreate: emailListToJSON,
+      beforeUpdate: emailListToJSON
+    }
   }
 };
 
@@ -20,12 +56,20 @@ export const Review = {
     reviewId: Sequelize.STRING,
     author: Sequelize.STRING,
     rating: Sequelize.DECIMAL,
-    staff: Sequelize.DECIMAL,
-    punctuality: Sequelize.DECIMAL,
-    helpfulness: Sequelize.DECIMAL,
-    knowledge: Sequelize.DECIMAL,
+    ratingBreakdown: {
+      type: Sequelize.TEXT,
+      get: function () {
+        safelyParseJSON(this.getDataValue('ratingBreakdown'));
+      }
+    },
     comment: Sequelize.TEXT,
     created: Sequelize.STRING
+  },
+  methods: {
+    hooks: {
+      beforeCreate: ratingBreakdownToJSON,
+      beforeUpdate: ratingBreakdownToJSON
+    }
   }
 };
 
